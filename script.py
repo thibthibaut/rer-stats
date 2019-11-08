db8ce962ef2539bd22fcb7d82e3846265d0f5691
Thibaut Vercueil <thibaut.vercueil@st.com>
2019-11-08 15:26:24 +0100

    Removed comments

--- script.py
+++ script.py
@@ -10,21 +10,21 @@ import os
 def get_tweets(api=None, screen_name=None):
 
     timeline = api.GetUserTimeline(screen_name=screen_name, count=200)
-    # earliest_tweet = min(timeline, key=lambda x: x.id).id
-    # print("getting tweets before:", earliest_tweet)
+    earliest_tweet = min(timeline, key=lambda x: x.id).id
+    print("getting tweets before:", earliest_tweet)
 
-    # while True:
-    #     tweets = api.GetUserTimeline(
-    #         screen_name=screen_name, max_id=earliest_tweet, count=200
-    #     )
-    #     new_earliest = min(tweets, key=lambda x: x.id).id
+    while True:
+        tweets = api.GetUserTimeline(
+            screen_name=screen_name, max_id=earliest_tweet, count=200
+        )
+        new_earliest = min(tweets, key=lambda x: x.id).id
 
-    #     if not tweets or new_earliest == earliest_tweet:
-    #         break
-    #     else:
-    #         earliest_tweet = new_earliest
-    #         print("getting tweets before:", earliest_tweet)
-    #         timeline += tweets
+        if not tweets or new_earliest == earliest_tweet:
+            break
+        else:
+            earliest_tweet = new_earliest
+            print("getting tweets before:", earliest_tweet)
+            timeline += tweets
 
     return timeline
 
@@ -37,7 +37,7 @@ if __name__ == "__main__":
     print(screen_name)
     timeline = get_tweets(api=api, screen_name=screen_name)
 
-    # with open('examples/timeline.json', 'w+') as f:
-    #     for tweet in timeline:
-    #         f.write(json.dumps(tweet._json))
-    #         f.write('\n')
+    with open('rerb_timeline.json', 'w+') as f:
+        for tweet in timeline:
+            f.write(json.dumps(tweet._json))
+            f.write('\n')
